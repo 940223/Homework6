f57bfc8d3a1627faae0c3f26b397e41ce1d9f891
張翔 <940223@gmail.com>
2021-05-19 11:50:49 +0800

    'hw6'

---
+++ src/TodoList.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import TodoApp from './TodoApp'
+
+class TodoList extends React.Component {
+
+    SubmitDelete (id) {
+       //console.log(id);
+        this.props.DeleteItemTop(id);
+      }
+    
+      ChangeDone (id) {
+        this.props.ChangeCompleteTop(id);
+      }
+
+  render () { 
+    console.log(this.props.data);
+    var value = this.props.choosevalue;
+    //var deleteFlag = this.props.data["deleteFlag"];
+    
+    const a = this.props.data.map(({id, text, complete, deleteFlag}, index) => {
+        if (value == '1' && deleteFlag !== true) {
+            return  <TodoApp 
+                            key={index} 
+                            id={id} 
+                            text={text} 
+                            complete={complete} 
+                            ChangeCompleteItem={this.ChangeDone.bind(this)}                         
+                            DeleteItem={this.SubmitDelete.bind(this)}
+                      />
+        }
+  
+        if (value == '2' && complete === false && deleteFlag !== true) {
+            return  <TodoApp 
+                            key={index} 
+                            id={id} 
+                            text={text} 
+                            complete={complete} 
+                            ChangeCompleteItem={this.ChangeDone.bind(this)}                         
+                            DeleteItem={this.SubmitDelete.bind(this)}
+                      />
+        }
+  
+        if (value == '3' && complete === true && deleteFlag !== true) {
+            return  <TodoApp 
+                            key={index} 
+                            id={id} 
+                            text={text} 
+                            complete={complete} 
+                            ChangeCompleteItem={this.ChangeDone.bind(this)}                         
+                            DeleteItem={this.SubmitDelete.bind(this)}
+                      />
+        }
+     })
+
+    return (
+      <div> { a } </div>
+    )
+  }
+}
+
+export default TodoList
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import App from './App'
+ 
+ let data = []
+ 
+ ReactDOM.render(
+   <App data={data}/>,
+   document.getElementById('root')
+ )
+
+
+
+
+ 
